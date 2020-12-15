d979de1e5a39ae7bdad431485683ff8d470668b5
Carlos Pedraza <capedraza@gmail.com>
2020-12-15 08:34:53 -0500

    Update 15/12/2020 Ejercicios ciclos

--- src/ciclos/Multas.java
+++ src/ciclos/Multas.java
@@ -1,7 +1,23 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+El departamento de policía de la ciudad ha acumulado información referente a las infracciones de los límites de velocidad durante un determinado período de tiempo. El departamento ha dividido la ciudad en cuatro cuadrantes y desea realizar una estadística de las infracciones a los límites de velocidad en cada uno de ellos. Para cada infracción se capturan los siguientes datos:
+
+Número de registro del vehículo (código numérico), cuadrante en el que se produjo la infracción (1 a 4), límite de velocidad permitido en km/h (entero), velocidad real desarrollada en km/h (entero).
+
+Diseñe un programa para producir dos informes; el primero, que contiene una lista de las multas de velocidad recolectadas, donde la multa se calcula como la suma del costo de la infracción ($50) más $20 por cada km/h que se ha excedido la velocidad límite. Por ejemplo:
+El vehículo con registro 76234 registró una velocidad de 85 km/h en una zona de 60 km/h, por lo que la multa se calculó de la siguiente forma: 50 + (85-60)*20 = 350
+Registro	Velocidad registrada	Velocidad límite	Multa
+76234	85	60	$350
+…..			
+Este informe debe ser seguido de un segundo, en el cual se proporciona un análisis de las infracciones por cuadrante. Para cada uno de los cuatro cuadrantes mencionados, debe darse el número de infracciones y la multa promedio. Por ejemplo:
+
+Cuadrante 1. Total de infracciones:         Multa promedio:   ___        
+….
+Cuadrante 4. Total de infracciones:         Multa promedio:  ____         
+
+La multa promedio se calcula sumando el monto de todas las multas en un cuadrante y dividiendo entre el número de multas en ese cuadrante.
+
+NOTA: Suponga que se acumularon datos de N infracciones.
+
  */
 package ciclos;
 
@@ -58,7 +74,5 @@ public class Multas {
         System.out.println("Cuadrante 2. No. Inf: "+ninf2+" Prom. Multas:"+ acmul2/ninf2);
         System.out.println("Cuadrante 3. No. Inf: "+ninf3+" Prom. Multas:"+ acmul3/ninf3);
         System.out.println("Cuadrante 4. No. Inf: "+ninf4+" Prom. Multas:"+ acmul4/ninf4);
-        
-
-    }
+      }
 }
